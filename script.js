0ae07095495bd10ab3bc54fec53b76a2a4a4394e
aryaabhi023 <abhishekparmar2306@gmail.com>
2023-11-19 04:45:24 +0530

    second commit

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const darkModeToggle = document.getElementById("darkModeToggle");
+const changer = document.getElementById("changer");
 const body = document.body;
 const logo = document.getElementById("logo");
 const isDarkMode = localStorage.getItem("darkMode") === "enabled";
 if (isDarkMode) {
   body.classList.add("dark-mode");
+  changer.classList.remove("bxs-sun");
+  changer.classList.add("bxs-moon");
   logo.classList.remove("logo-dark");
   darkModeToggle.checked = true;
 }
 darkModeToggle.addEventListener("change", () => {
   if (darkModeToggle.checked) {
+    changer.classList.remove("bxs-sun");
+    changer.classList.add("bxs-moon");
     logo.classList.remove("logo-dark");
     body.classList.add("dark-mode");
     localStorage.setItem("darkMode", "enabled");
   } else {
+    changer.classList.remove("bxs-moon");
+    changer.classList.add("bxs-sun");
     logo.classList.add("logo-dark");
     body.classList.remove("dark-mode");
     localStorage.setItem("darkMode", "disabled");
